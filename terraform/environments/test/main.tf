aec646f57f04016ca56e085c939d066b8acc002b
jared.magrath <magrathj@tcd.ie>
2021-01-17 20:01:56 +0000

    wrong commit reverted

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -6,12 +6,6 @@ provider "azurerm" {
   features {}
 }
 
-backend "azurerm"  {
-    storage_account_name = "tstate21134"
-    container_name       = "tstate"
-    key                  = "terraform.tfstate"
-    access_key           = ""
-  }
 
 terraform {
   backend "azurerm" {
